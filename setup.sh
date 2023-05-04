dd46faec7ffdd5f43d7402cd115761d0f411d4d2
John Rieffel <rieffelj@union.edu>
2023-05-04 19:52:47 -0400

    fixes

--- setup.sh
+++ setup.sh
@@ -2,16 +2,19 @@
 
 cd /autograder/source
 
-apt-get install -y python python-pip python-dev
+apt-get install -y python3 python3-pip python3-dev
+
+ls -al
 
 mkdir -p /root/.ssh
 cp ssh_config /root/.ssh/config
 # Make sure to include your private key here
 cp deploy_key /root/.ssh/deploy_key
+chmod 400 /root/.ssh/deploy_key
 # To prevent host key verification errors at runtime
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
 # Clone autograder files
 git clone git@github.com:jrieffel/10x-autograding.git
 # Install python dependencies
-pip install -r /autograder/10x-autograding/requirements.txt
+pip install gradescope-utils
