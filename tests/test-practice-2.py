127bfe37da1bc45e0c7e6968792b7a66470a8500
John Rieffel <rieffelj@union.edu>
2023-05-04 19:05:06 -0400

    checkin

---
+++ tests/test-practice-2.py
@@ -0,0 +1,44 @@
+import unittest
+#from unittest import mock
+import re
+import sys
+from io import StringIO
+from gradescope_utils.autograder_utils.decorators import weight, tags
+from unittest import mock
+import subprocess
+import math
+
+
+class TestWeek8(unittest.TestCase):
+
+    MARKER = "### DO NOT DELETE THIS LINE: beg testing"
+    STUDENT = {} # namespace of student code
+
+    @classmethod
+    def setUpClass(cls):
+         try:
+             student_code = open('practice-quiz-2.py','rt').read()
+             student_code = student_code.split(cls.MARKER)[0]
+             exec(student_code, cls.STUDENT)
+         except:
+             exec("", cls.STUDENT)
+
+
+
+
+
+
+    @weight(1)
+    def test_sum_100(self):
+        ''' 
+        Sum100([1,2,3,4,5]) should return 15
+        '''
+        funName = 'Sum100'
+        testval1 = [1,2,3,4,5]
+        expected = 15
+        msg = "Sum100([1,2,3,4,5]) should return 15"
+        result = TestWeek8.STUDENT[Sum100](testval1,testval2)
+        self.assertEqual(result, expected, msg=msg)
+
+
+     
